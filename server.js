b7645d10511403c3e7ee43e0223fac0de804b31c
정윤식 <wjddbstlr11@naver.com>
2024-06-17 20:15:18 +0900

    서버 수정

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const { MongoClient, ObjectId } = require('mongodb');
 const methodOverride = require('method-override');
 const bcrypt = require('bcrypt');
-
 const { createServer } = require('http');
+// const chatNotificationRouter = require('./routes/chatNotification');
 const { Server } = require('socket.io');
 const server = createServer(app);
-const io = new Server(server); 
+const io = new Server(server);
 
 require('dotenv').config();
 
@@ -15,11 +15,12 @@ app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+// app.use('/chat', chatNotificationRouter);
 
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const MongoStore = require('connect-mongo');
 
 const sessionMiddleware = session({
@@ -28,73 +29,89 @@ const sessionMiddleware = session({
     saveUninitialized: false,
     cookie: { maxAge: 60 * 60 * 1000 },
     store: MongoStore.create({
-      mongoUrl: process.env.DB_URL,
-      dbName: 'forum'
+        mongoUrl: process.env.DB_URL,
+        dbName: 'forum'
     })
 });
 
 app.use(sessionMiddleware);
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 
 const sharedSession = require('express-socket.io-session');
 io.use(sharedSession(sessionMiddleware, {
-  autoSave: true
+    autoSave: true
 }));
 
 const upload = require('./upload.js');
-
 let connectDB = require('./database.js');
 
 let db;
-let changeStream;
-connectDB.then((client)=>{
-  console.log('DB연결성공')
-  db = client.db('forum');
+let postListener;
+let chatListener;
 
-  let 조건 = [
-    { $match : { operationType : 'insert' } }
-    ]
-    changeStream = db.collection('post').watch(조건)
-
-  server.listen(process.env.PORT, () => {
-    console.log('http://localhost8080 에서 서버 실행중')
+connectDB.then((client) => {
+    console.log('DB연결성공');
+    db = client.db('forum');
+    server.listen(process.env.PORT, () => {
+        console.log('http://localhost8080 에서 서버 실행중');
     });
-}).catch((err)=>{
-  console.log(err)
+
+    const postStream = [{ $match: { operationType: 'insert' } }];
+    const chatStream = [{ $match: { operationType: 'insert' } }];
+
+    // 게시글 리스너 등록
+    if (!postListener) {
+        postListener = db.collection('post').watch(postStream);
+        postListener.on('change', (change) => {
+            console.log('게시글 change event:', change.fullDocument);
+            io.emit('postChange', change.fullDocument);
+        });
+    }
+
+    // 채팅 리스너 등록
+    if (!chatListener) {
+        chatListener = db.collection('message').watch(chatStream);
+        chatListener.on('change', async (change) => {
+            console.log('채팅 change event:', change.fullDocument);
+            // 변경 이벤트가 발생한 메시지를 전송한 사용자 ID를 제외하고 이벤트 전송
+            sendEvent('chatChange', change.fullDocument, change.fullDocument.senderId);
+        });
+    }
+}).catch((err) => {
+    console.error('DB 연결 오류:', err);
 });
 
+if (process.env.NODE_ENV === 'development') {
+    console.log('현재는 개발 환경입니다.');
+}
 
-function 로그인 (요청, 응답, next) {
+function 로그인(요청, 응답, next) {
     if (!요청.user) {
-        return 응답.send('로그인하세요')
+        return 응답.send('로그인하세요');
     }
-    next()
-};
+    next();
+}
 
-function 아이디비번체크 (요청, 응답, next) {
+function 아이디비번체크(요청, 응답, next) {
     if (요청.body.username == '' || 요청.body.password == '') {
-      응답.send('그러지마세요')
+        응답.send('그러지마세요');
     } else {
-      next()
+        next();
     }
-} 
+}
 
 app.get('/', (요청, 응답) => {
-    응답.render('index.ejs')
+    응답.render('index.ejs');
 });
-
 
 app.get('/about', (요청, 응답) => {
-    응답.sendFile(__dirname + '/about.html')
+    응답.sendFile(__dirname + '/about.html');
 });
 
-
 app.get('/time', async (요청, 응답) => {
-    응답.render('time.ejs', { data : new Date() })
-})
-
-
+    응답.render('time.ejs', { data: new Date() });
+});
 
 app.delete('/doc', async (요청, 응답) => {
     try {
@@ -109,102 +126,101 @@ app.delete('/doc', async (요청, 응답) => {
     }
 });
 
-
 passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
-    let result = await db.collection('user').findOne({ username : 입력한아이디})
+    let result = await db.collection('user').findOne({ username: 입력한아이디 });
     if (!result) {
-      return cb(null, false, { message: '아이디 DB에 없음' })
+        return cb(null, false, { message: '아이디 DB에 없음' });
     }
 
     if (await bcrypt.compare(입력한비번, result.password)) {
-      return cb(null, result)
+        return cb(null, result);
     } else {
-      return cb(null, false, { message: '비번불일치' });
+        return cb(null, false, { message: '비번불일치' });
     }
 }));
 
 passport.serializeUser((user, done) => {
     process.nextTick(() => {
-      done(null, { id: user._id, username: user.username })
-    })
-  });
+        done(null, { id: user._id, username: user.username });
+    });
+});
 
 passport.deserializeUser(async (user, done) => {
-    let result = await db.collection('user').findOne({_id : new ObjectId(user.id)})
+    let result = await db.collection('user').findOne({ _id: new ObjectId(user.id) });
     const userObject = { id: result._id, username: result.username };
     process.nextTick(() => {
         done(null, userObject);
     });
-  });
+});
 
-
-
-
-
+app.get('/logout', (요청, 응답) => {
+    요청.logout((err) => {
+        if (err) { return next(err); }
+        요청.session.destroy((err) => {
+            if (err) {
+                console.log(err);
+                return 응답.send('로그아웃 중 에러 발생');
+            }
+            응답.redirect('/');
+        });
+    });
+});
 
 app.get('/detail/:id', async (요청, 응답) => {
-    console.log(요청.user.username)
+    // console.log(요청.user.username)
     try {
-        let result2 = await db.collection('comment').find( { parentId : new ObjectId(요청.params.id) } ).toArray()
-        let result = await db.collection('post').findOne({ _id : new ObjectId(요청.params.id) })
+        let result2 = await db.collection('comment').find({ parentId: new ObjectId(요청.params.id) }).toArray();
+        let result = await db.collection('post').findOne({ _id: new ObjectId(요청.params.id) });
         if (result == null) {
-            응답.status(404).send('존재하지 않는 게시물입니다. 주소가 맞는지 다시 한번 확인해주세요.')
+            응답.status(404).send('존재하지 않는 게시물입니다. 주소가 맞는지 다시 한번 확인해주세요.');
         }
-        응답.render('detail.ejs', { result : result, result2 : result2, 유저 : 요청.user.username })
+
+        let 유저 = 요청.user ? 요청.user.username : undefined;
+
+        응답.render('detail.ejs', { result: result, result2: result2, 유저: 유저 });
     }
     catch (e) {
-        console.log(e)
-        응답.status(404).send('잘못된 주소입니다')
+        console.log(e);
+        응답.status(404).send('잘못된 주소입니다');
     }
 });
 
 
 
 app.get('/mypage', async (요청, 응답, next) => {
-    let 회원아이디 = 요청.user
-    console.log(회원아이디)
+    let 회원아이디 = 요청.user;
+    console.log(회원아이디);
     if (회원아이디) {
-        응답.render('mypage.ejs', {회원아이디 : 회원아이디})
+        응답.render('mypage.ejs', { 회원아이디: 회원아이디 });
+    } else {
+        응답.send('로그인 안했는데요');
     }
-    else {
-        응답.send('로그인 안했는데요')
-    }
-    
-})
+});
 
-app.get('/write', 로그인, async (요청,응답) => {
-    응답.render('write.ejs')
-})
+app.get('/write', 로그인, async (요청, 응답) => {
+    응답.render('write.ejs');
+});
 
-app.get('/search', async (요청,응답) => {
-    console.log(요청.query.val)
+app.get('/search', async (요청, 응답) => {
+    console.log(요청.query.val);
     let 검색조건 = [
-        {$search : {
-          index : 'title_index',
-          text : { query : 요청.query.val, path : 'title' }
-        }},
-        { $limit : 5 },
-        
-      ]
-    let result = await db.collection('post').aggregate(검색조건).toArray()
-    응답.render('search.ejs', { 글목록 : result })
-})
+        { $search: { index: 'title_index', text: { query: 요청.query.val, path: 'title' } } },
+        { $limit: 5 },
+    ];
+    let result = await db.collection('post').aggregate(검색조건).toArray();
+    응답.render('search.ejs', { 글목록: result });
+});
 
-
-
-app.post('/newpost', upload.array('img1'), async (요청, 응답)=> {
-    
+app.post('/newpost', upload.array('img1'), async (요청, 응답) => {
     try {
         if (요청.files.length > 8) {
             return 응답.status(400).send('이미지는 최대 7개까지 업로드할 수 있습니다.');
         }
         if (요청.body.title == '') {
-            응답.send('제목을 입력하세요')
-        }
-        else if (요청.body.content == '') {
-            응답.send('내용을 입력하세요')
-        }
-        else {
+            응답.send('제목을 입력하세요');
+        } else if (요청.body.content == '') {
+            응답.send('내용을 입력하세요');
+        } else {
             const imgUrls = 요청.files.map(file => file.location);
             await db.collection('post').insertOne({
                 title: 요청.body.title,
@@ -214,43 +230,35 @@ app.post('/newpost', upload.array('img1'), async (요청, 응답)=> {
                 username: 요청.user.username
             });
             응답.redirect('/list');
-        }  
+        }
+    } catch (e) {
+        console.log(e);
+        응답.status(500).send('서버 에러');
     }
-    catch (e) {
-        console.log(e)
-        응답.status(500).send('서버 에러')
-    }
-} );
-
-app.post('/comment', 로그인, async (요청, 응답) => {
-    console.log(요청.user)
-    
-    await db.collection('post').findOne(new ObjectId)
-    await db.collection('comment').insertOne({
-        content : 요청.body.content,
-        writerId : new ObjectId(요청.user.id),
-        writer : 요청.user.username,
-        parentId : new ObjectId(요청.body.parentId)
-    })
-    응답.redirect('back')
 });
 
+app.post('/comment', 로그인, async (요청, 응답) => {
+    console.log(요청.user);
+    
+    await db.collection('post').findOne(new ObjectId);
+    await db.collection('comment').insertOne({
+        content: 요청.body.content,
+        writerId: new ObjectId(요청.user.id),
+        writer: 요청.user.username,
+        parentId: new ObjectId(요청.body.parentId)
+    });
+    응답.redirect('back');
+});
 
+io.on('connection', (socket) => {
+    if (socket.handshake.session.passport && socket.handshake.session.passport.user) {
+        socket.userId = socket.handshake.session.passport.user.id;
+    }
 
-
-
-
-
-io.on('connection', (socket)=> {
-    // 이 부분에서 socket.request.session.passport.user.id를 이용하여 senderId를 설정합니다.
-    const senderId = socket.handshake.session.passport.user.id;
-
-    socket.on('ask-join', (data)=>{
-        // socket.request.session
-        socket.join(data)
+    socket.on('ask-join', (data) => {
+        socket.join(data);
     });
 
-    // 클라이언트로부터 이전 메시지 요청을 받는 핸들러
     socket.on('request-previous-messages', async (roomId) => {
         try {
             const messages = await db.collection('message').find({ documentId: roomId }).toArray();
@@ -260,35 +268,25 @@ io.on('connection', (socket)=> {
         }
     });
 
-    socket.on('message-send', async (data)=>{
-        console.log(data);
+    socket.on('message-send', async (data) => {
         try {
             const roomId = new ObjectId(data.room);
-            // 채팅방의 _id를 사용하여 채팅방을 찾음
-            let result = await db.collection('chatroom').findOne({ _id : roomId });
-            if(result) {
-                const chatId = result.member; 
-                await db.collection('message').insertOne({
-                    chatContent : data.msg,
-                    date : new Date(),
-                    chatId : chatId,
-                    documentId : data.room,
-                    senderId: senderId 
-                });
-                // 해당 채팅방의 멤버에게만 메시지를 전송
-                chatId.forEach(memberId => {
-                    io.to(memberId).emit('message-broadcast', { 
-                        chatContent: data.msg, 
-                        chatId: chatId, 
-                        senderId: senderId
-                    }); // 채팅 아이디 전송
-                });
-                
+            let result = await db.collection('chatroom').findOne({ _id: roomId });
+            if (result) {
+                const chatId = result.member;
+                const message = {
+                    chatContent: data.msg,
+                    date: new Date(),
+                    chatId: chatId,
+                    documentId: data.room,
+                    senderId: socket.userId 
+                };
+                await db.collection('message').insertOne(message);
 
-                // 이전 메시지를 가져오는 로직
-                const messages = await db.collection('message').find({ documentId: data.room }).toArray();
-                // 클라이언트로 메시지 전송
-                io.to(data.room).emit('previous-messages', messages);
+                // chatId.forEach(memberId => {
+                //     io.to(memberId).emit('message-broadcast', message);
+                // });
+                io.to(data.room).emit('message-broadcast', message);
             } else {
                 console.log('채팅방을 찾을 수 없습니다.');
             }
@@ -296,42 +294,79 @@ io.on('connection', (socket)=> {
             console.error('채팅방 ID를 ObjectId로 변환하는 동안 오류가 발생했습니다:', error);
         }
     });
+
+    socket.on('delete-message', async ({ messageId, roomId }) => {
+        try {
+            const result = await db.collection('message').updateOne(
+                { _id: new ObjectId(messageId), documentId: roomId },
+                { $set: { chatContent: '메시지가 삭제되었습니다.' } }
+            );
+            if (result.modifiedCount === 1) {
+                socket.emit('message-deleted', { messageId: messageId, roomId: roomId });
+            } else {
+                console.log('메시지 삭제 실패');
+            }
+        } catch (error) {
+            console.error('메시지 삭제 중 오류 발생:', error);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('소켓 연결 종료:', socket.id);
+    });
 });
 
-app.get('/stream/list', (요청, 응답)=>{
+const sendEvent = (eventName, data, excludeUserId) => {
+    io.sockets.sockets.forEach(socket => {
+        if (socket.userId !== excludeUserId) {
+            socket.emit(eventName, data);
+        }
+    });
+};
+
+app.get('/stream/list', (요청, 응답) => {
     응답.writeHead(200, {
         "Connection": "keep-alive",
         "Content-Type": "text/event-stream",
         "Cache-Control": "no-cache",
-    })
-    
+    });
 
-        changeStream.on('change', (result)=>{
-            console.log(result.fullDocument)
-            응답.write('event: msg\n');
-            응답.write(`data: ${JSON.stringify(result.fullDocument)}\n\n`);
-        })
-})
+    const sendEvent = (eventName, data) => {
+        응답.write(`event: ${eventName}\n`);
+        응답.write(`data: ${JSON.stringify(data)}\n\n`);
+    };
 
+    요청.on('close', () => {
+        console.log('클라이언트 연결 종료');
+        if (postListener) {
+            postListener.removeListener('change', handlePostChange);
+        }
+        if (chatListener) {
+            chatListener.removeListener('change', handleChatChange);
+        }
+        응답.end();
+    });
 
+    const handlePostChange = (change) => {
+        sendEvent('postChange', change.fullDocument);
+    };
 
+    const handleChatChange = (change) => {
+        sendEvent('chatChange', change.fullDocument);
+    };
+
+    if (postListener) {
+        postListener.on('change', handlePostChange);
+    }
+    if (chatListener) {
+        chatListener.on('change', handleChatChange);
+    }
+});
 
 app.use('/shop', require('./routes/shop.js'));
-
 app.use('/board/sub', require('./routes/board.js'));
-
 app.use('/login', require('./routes/login.js'));
-
 app.use('/edit', require('./routes/edit.js'));
-
 app.use('/list', require('./routes/list.js'));
-
 app.use('/register', require('./routes/register.js'));
-
 app.use('/chat', require('./routes/chat.js'));
-
-
-
-
-
-
